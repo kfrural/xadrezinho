576cfc7267080ced830391cbecfd7432f05e73b2
Karla C. Ferreira <karlinharural@gmail.com>
2024-01-02 16:04:34 -0300

    Update ChessGame.h

--- ChessOrientado/ChessGame.h
+++ ChessOrientado/ChessGame.h
@@ -6,19 +6,24 @@
 
 using namespace std;
 
-extern char tabuleiro[8][8];
-extern int l_origem, l_destino, c_origem, c_destino;
+class ChessGame {
+    public:
+        void imprimirTabuleiro(char peca[8][8]);
+        void criarTabuleiro();
+        bool verificaJogadorTimeCorreto(int l, int c, int player);
+        bool verificarMovimentoPeao(int l_origem, int c_origem, int l_destino, int c_destino);
+        bool verificarMovimentoTorre(int l_origem, int c_origem, int l_destino, int c_destino);
+        bool verificarMovimentoCavalo(int l_origem, int c_origem, int l_destino, int c_destino);
+        bool verificarMovimentoBispo(int l_origem, int c_origem, int l_destino, int c_destino);
+        bool verificarMovimentoRainha(int l_origem, int c_origem, int l_destino, int c_destino);
+        bool verificarMovimentoRei(int l_origem, int c_origem, int l_destino, int c_destino);
+        void escolhaJogada(int player);
+        void movimentoDaJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
 
-void imprimirTabuleiro(char peca[8][8]);
-void criarTabuleiro();
-bool verificaJogadorTimeCorreto(int l, int c, int player);
-bool verificarMovimentoPeao(int l_origem, int c_origem, int l_destino, int c_destino);
-bool verificarMovimentoTorre(int l_origem, int c_origem, int l_destino, int c_destino);
-bool verificarMovimentoCavalo(int l_origem, int c_origem, int l_destino, int c_destino);
-bool verificarMovimentoBispo(int l_origem, int c_origem, int l_destino, int c_destino);
-bool verificarMovimentoRainha(int l_origem, int c_origem, int l_destino, int c_destino);
-bool verificarMovimentoRei(int l_origem, int c_origem, int l_destino, int c_destino);
-void escolhaJogada(int player);
-void movimentoDaJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
+    
+        char tabuleiro[8][8];
+        int l_origem, l_destino, c_origem, c_destino;
+
+};
 
 #endif
